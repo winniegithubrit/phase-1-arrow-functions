9b3d477d2f8fb668c19162e430213c8143f4bc80
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-03-15 16:21:55 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = () => 2000 / 100;
+const square = (x)=>{
+  return (x**2)  
+}
+console.log(square(2));
+const add = (num3 , num4)=>{
+  return (num3 + num4);
+
+}
+console.log(add(num3,num4));
